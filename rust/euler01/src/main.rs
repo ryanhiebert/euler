4d31fe57d4ef7676c6faa2aa0da5909961759e33
Ryan Hiebert <ryan@ryanhiebert.com>
2015-02-07 15:45:27 -0600

    Do euler 1 in rust

---
+++ rust/euler01/src/main.rs
@@ -0,0 +1,11 @@
+#![feature(core)]
+
+fn main() {
+    let mut sum = 0;
+    for i in 0..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+    println!("{}", sum);
+}
